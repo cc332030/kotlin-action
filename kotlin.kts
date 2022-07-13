d0cd0aff3e699b6d246189c23b3b938e172d2976
c332030 <c332030@gmail.com>
2022-07-13 16:21:48 +0800

    feat: Hellow World

--- kotlin.kts
+++ kotlin.kts
@@ -1,7 +1,2 @@
 
-val current = LocalDateTime.now()
-
-val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")
-val formatted = current.format(formatter)
-
-println("Hellow World, current time: ${formatted}")
+println("Hellow World")
