d984cf0cde592690525bea2c0e936ca1c68614a9
c332030 <c332030@gmail.com>
2022-07-14 09:27:20 +0800

    feat: KOTLIN_PACKAGE_NAME

--- init.sh
+++ init.sh
@@ -9,13 +9,15 @@ downUrl=$(curl -s https://api.github.com/repos/JetBrains/kotlin/releases/latest 
 
 echo "downUrl: $downUrl"
 
+KOTLIN_PACKAGE_NAME=kotlin-compiler.zip
+
 curl -sL \
   "$downUrl" \
-  -o kotlin-compiler.zip
+  -o $KOTLIN_PACKAGE_NAME
 
 # -o 覆盖已有文件，-q 静默
-unzip -oq kotlin-compiler.zip -d /usr/local
-rm -f kotlin-compiler.zip
+unzip -oq $KOTLIN_PACKAGE_NAME -d /usr/local
+rm -f $KOTLIN_PACKAGE_NAME
 
 # 创建软链接
 for f in /usr/local/kotlinc/bin/*; do
